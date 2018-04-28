311da0d2ba637f1dde910be722277c1bc8624840
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-28 16:45:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function createPost() {
   
   postDivs.innerHTML += postTemplateHTML;
 }
+
+function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+ 
+  let commentTemplate = document.getElementById("comment-template").innerHTML;
+  
+  let templateFn = _.template(commentTemplate);
+
+  let commentsDiv = document.getElementById("comments");
+
+  let templateHTML = templateFn({ 'comment': comment, 'commenter': commenter});
+
+  commentsDiv.innerHTML += templateHTML;
+}
