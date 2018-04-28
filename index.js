b59128cd780a077d35ed5a9a505adf3875df4510
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-28 17:00:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function createPost() {
   
   let pageTemplate = _.template(document.getElementById('page-template').innerHTML);
   let postTemplate = _.template(document.getElementById('post-template').innerHTML);
-  let commentsTemplate = _.template(document.getElementById('commentsTemplate').innerHTML);
+  let commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
   
   document.getElementsByTagName('main')[0].innerHTML += pageTemplate;
   
